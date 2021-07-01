6a6628fad9e6dd07c78c520bb28e997e8098416f
zubair-pc\zubair <szubair@gmail.com>
2021-07-01 17:52:48 +0500

    Product filter inital strcutre is created on login color is changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/componenents/home/home.component';
 import { LoginComponent } from './core/componenents/login/login.component';
+import { ProductFilterComponent } from './shopping/components/products/product-filter/product-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ProductFilterComponent
   ],
   imports: [
     BrowserModule,
